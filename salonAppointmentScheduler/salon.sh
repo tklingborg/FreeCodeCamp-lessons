3a01a6b57ea9be8b861b73cbf8dd9e67ac5b9797
tklingborg <49267913+tklingborg@users.noreply.github.com>
2023-06-01 16:07:55 +0200

    initial commit

---
+++ salonAppointmentScheduler/salon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+MAIN_MENU() {
+  echo -e "\nWelcome to My Salon, how can I help you?"
+  # list the services offered
+  # read selected service
+  # if not exist
+  # send to main menu
+}
